16732b44d758320b757a5f74aca33ce54eceaca1
a <pswxsj@naver.com>
2023-02-15 04:51:51 +0900

    Delete loss plot

--- train.py
+++ train.py
@@ -44,34 +44,34 @@ if __name__ == '__main__':
         for i, (images, labels) in enumerate(train_dataloader, 0):
             images, labels = images.to(device), labels.to(device)
             running_train_loss += loss_function(model(images), labels).item() / images.shape[0]
-        for i, (images, labels) in enumerate(test_dataloader, 0):
-            images, labels = images.to(device), labels.to(device)
-            running_test_loss += loss_function(model(images), labels).item() / images.shape[0]
-            print(running_test_loss)
+        # for i, (images, labels) in enumerate(test_dataloader, 0):
+        #     images, labels = images.to(device), labels.to(device)
+        #     running_test_loss += loss_function(model(images), labels).item() / images.shape[0]
+        #     print(running_test_loss)
         train_losses.append(running_train_loss)
-        test_losses.append(running_test_loss)
+        # test_losses.append(running_test_loss)
         # running_loss = 0.0
         # for i, data in enumerate(train_loader, 0):
         #     running_loss +=
-    model.eval()
-    test_loss, correct, total = 0, 0, 0
-    for i, (images, labels) in enumerate(test_dataloader):
-        images, labels = images.to(device), labels.to(device)
-
-        output = model(images)
-        test_loss += loss_function(output, labels).item()
-
-        pred = output.max(1, keepdim=True)[1]
-        correct += pred.eq(labels.view_as(pred)).sum().item()
-
-        total += labels.size(0)
-
-    print('[Test set] Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        test_loss / total, correct, total,
-        100. * correct / total))
-    plt.plot(test_losses, label="test_loss")
-    plt.plot(train_losses, label="train_loss")
-    plt.legend()
-    plt.show()
+    # model.eval()
+    # test_loss, correct, total = 0, 0, 0
+    # for i, (images, labels) in enumerate(test_dataloader):
+    #     images, labels = images.to(device), labels.to(device)
+    #
+    #     output = model(images)
+    #     test_loss += loss_function(output, labels).item()
+    #
+    #     pred = output.max(1, keepdim=True)[1]
+    #     correct += pred.eq(labels.view_as(pred)).sum().item()
+    #
+    #     total += labels.size(0)
+    #
+    # print('[Test set] Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+    #     test_loss / total, correct, total,
+    #     100. * correct / total))
+    # plt.plot(test_losses, label="test_loss")
+    # plt.plot(train_losses, label="train_loss")
+    # plt.legend()
+    # plt.show()
 
     torch.save(model.state_dict(), 'model.pt')
