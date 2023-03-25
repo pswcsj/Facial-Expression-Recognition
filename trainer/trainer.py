b7fb8f6880a4a7746248c26a56af1931a0aa6c7c
a <pswxsj@naver.com>
2023-03-25 16:40:01 +0900

    Order change in trainer.py cause of before assignment

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -6,8 +6,9 @@ from model.loss import robust_loss
 
 def train(model, epochs, lr, train_dataloader, test_dataloader):
     total_train_loss, train_correct, train_total = 0, 0, 0
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=gamma)
     optimizer = RobustOptimizer(filter(lambda p: p.requires_grad, model.parameters()), optim.Adam, lr=lr)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=gamma)
+
     for epoch in range(epochs):
         model.train()
         for images, labels in tqdm(train_dataloader):
